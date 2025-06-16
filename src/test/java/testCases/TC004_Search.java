c051eaeb3f85f1bd8c820ce4dd551e3981183bd2
sanyogbisen91 <sanyogbisen91@gmail.com>
2025-06-16 10:06:10 +0530

    second commit

--- src/test/java/testCases/TC004_Search.java
+++ src/test/java/testCases/TC004_Search.java
@@ -1,5 +1,15 @@
 package testCases;
 
-public class TC004_Search {
+import org.testng.annotations.Test;
 
+public class TC004_Search {
+	
+	@Test //Step8 groups added
+	public void test_Search_Functionality() {
+		System.out.println("new test class added");
+		System.out.println("new line added for 2nd commit");
+	}
+	
+	
 }
+
